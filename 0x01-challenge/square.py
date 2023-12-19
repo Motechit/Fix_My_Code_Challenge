0c7c507fd68bb8f1c1c5de6c8809dd7ec0451999
Motechit <otikomosunmola@gmail.com>
2023-12-19 05:46:21 -0800

    second task

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
 
     width = 0
     height = 0
@@ -13,15 +13,14 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PerimeterOfMySquare(self):
-        return (self.width * 2) * (self.height * 2)
+    def perimeter_of_my_square(self):
+        return 4 * self.width
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
-
+        return "Square({}, {})".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PerimeterOfMySquare())
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
